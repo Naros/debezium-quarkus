103d973ce4b5e2e938c823fc76e106535c73a600
Chris Cranford <chris@hibernate.org>
2019-12-19 17:15:26 -0500

    Initial import

---
+++ extensions/outbox/runtime/src/test/java/io/debezium/quarkus/outbox/OutboxEventTest.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.quarkus.outbox;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Chris Cranford
+ */
+public class OutboxEventTest {
+    @Test
+    public void shouldTest() {
+        // do something here
+    }
+}
